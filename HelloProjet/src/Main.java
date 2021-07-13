c50147b4ba3a0094aefbc6605941dac99d67ccfb
luoting <lt1030666@163.com>
2021-07-13 11:20:30 +0800

    修改了Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -2,8 +2,7 @@ import org.junit.Test;
 
 public class Main {
 
-    @Test
-    public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!222222222");
     }
 }
