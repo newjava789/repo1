44b5dccde27ffafe9e6fbe18bab8311bf68b380c
Freedom on the top <63757340+newjava789@users.noreply.github.com>
2021-07-13 11:24:21 +0800

    Update Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -3,6 +3,6 @@ import org.junit.Test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld!222222222");
+        System.out.println("HelloWorld!33333333");
     }
 }
